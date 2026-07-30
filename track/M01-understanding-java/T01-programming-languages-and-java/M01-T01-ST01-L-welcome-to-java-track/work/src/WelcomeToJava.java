baa4c8cab54b750bf9db94a70a6ee362909974ca
fahadk426 <fahad32423khan5426@gmail.com>
2026-07-30 13:10:38 +0530

    added line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,5 +3,6 @@ public class WelcomeToJava {
        System.out.println("Welcome to java");
        System.out.println("I already know how to build programming");
        System.out.println("Now I am learning how to express that logic using java");
+       System.out.println("Lets start");
     }
 }
